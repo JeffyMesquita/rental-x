6d372bd74d6edfa0f05af35362545355fbc0adb1
Jeferson Mesquita <je_2742@hotmail.com>
2022-09-29 22:52:50 -0300

    a

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -14,22 +14,22 @@ categoriesRoutes.post('/', (request, response) => {
   const category = createCategoryService.execute({name, description});
 
   
-  // if(category !== {} as Category) {
-  //   console.log("already exists", category);
-  //   return;
-  // }
+  if(category !== {} as Category) {
+    console.log("already exists", category);
+    return;
+  }
 
-  // const categoryAlreadyExists = categoriesRepository.findByName(name);
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-  // if (categoryAlreadyExists) {
-  //   return response.status(400).json({
-  //     statusCode: 400,
-  //     result: 'error',
-  //     message: 'Category already exists',
-  //   });
-  // }
+  if (categoryAlreadyExists) {
+    return response.status(400).json({
+      statusCode: 400,
+      result: 'error',
+      message: 'Category already exists',
+    });
+  }
 
-  // const category = categoriesRepository.create({ name, description });
+  const category = categoriesRepository.create({ name, description });
 
   return response.status(201).json({
     statusCode: 201,
