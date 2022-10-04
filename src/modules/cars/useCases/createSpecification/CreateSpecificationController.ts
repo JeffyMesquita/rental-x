1185ee2163ed88a0a8433e946eea0a93c23856c9
Jeferson Mesquita <je_2742@hotmail.com>
2022-10-04 06:11:45 -0300

    Criando useCase pra importar categorias

--- src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -1,12 +1,20 @@
-import { Request, Response} from 'express';
+import { Request, Response } from 'express';
 import { CreateSpecificationUseCase } from './CreateSpecificationUseCase';
 
 class CreateSpecificationController {
-  handle(request: Request, response: Response) : Response {
-    const { name, description } = request.body;
-    const createSpecificationUseCase = new CreateSpecificationUseCase();
+  constructor(private createSpecificationUseCase: CreateSpecificationUseCase) {}
 
-    return response
+  handle(request: Request, response: Response): Response {
+    const { name, description } = request.body;
+
+    const specification = this.createSpecificationUseCase.execute({ name, description });
+
+    return response.status(201).json({
+      statusCode: 201,
+      result: 'success',
+      message: 'successfully created specification',
+      data: specification,
+    });
   }
 }
 
