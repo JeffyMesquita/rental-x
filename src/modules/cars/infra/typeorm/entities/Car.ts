c3fad58f1ed788d881dd6d3fd62d2dc4c0351d16
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2022-10-20 17:12:05 -0300

    criando Migrations Many to Many

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,6 +1,16 @@
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  ManyToMany,
+  JoinTable,
+  PrimaryColumn,
+} from 'typeorm';
 import { v4 as uuidV4 } from "uuid";
 import { Category } from "./Category";
+import { Specification } from './Specification';
 
 @Entity('cars')
 class Car {
@@ -29,7 +39,7 @@ class Car {
   brand: string;
 
   @ManyToOne(() => Category)
-  @JoinColumn({ name: 'category_id'})
+  @JoinColumn({ name: 'category_id' })
   category: Category;
 
   @Column()
@@ -38,8 +48,16 @@ class Car {
   @CreateDateColumn()
   created_at: Date;
 
-  constructor(){
-    if(!this.id) {
+  @ManyToMany(() => Specification)
+  @JoinTable({
+    name: 'specifications_cars',
+    joinColumns: [{ name: 'car_id' }],
+    inverseJoinColumns: [{ name: 'specification_id' }],
+  })
+  specifications: Specification[];
+
+  constructor() {
+    if (!this.id) {
       this.id = uuidV4();
       this.available = true;
     }
